b9c0d8ea8d920ed8201bb909bc614757dbc7db7e
tevfikyildiz <tevfikyildz@gmail.com>
2024-11-28 19:32:24 +0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,48 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { FaThumbsUp } from "react-icons/fa";
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [pictures, setPictures] = useState([]);
+  const [id,setId] =useState(0)
+  const handleAddPicture = () => {
+    const randomSeed = Math.random(); 
+    const newPicture = {
+      id:id,
+      img: `https://picsum.photos/200/300?random=${randomSeed}`, 
+      thumbClicked: false,
+    };
+    setPictures([...pictures, newPicture]);
+    setId(id+1)
+  };
+  const handleChange = (id) => {
+    setPictures(pictures.map((picture) =>
+      picture.id === id
+        ? { ...picture, thumbClicked: !picture.thumbClicked }
+        : picture
+    ));
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    
+  };
+
+  return (
+    <div>
+      <button onClick={handleAddPicture} className="button">Resim Ekle</button>
+      <div className="pictures">
+        {pictures.map((picture) => (
+          <div key={crypto.randomUUID()}>
+            <img
+            id={id}
+            onClick={() => handleChange(picture.id)}
+              src={picture.img}
+              className={"imgCustom"}
+              alt="Uploaded"
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <FaThumbsUp className={picture.thumbClicked ? "thumbClick" : "thumb"} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
